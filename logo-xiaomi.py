4277c394d8b1a35a41bbd8d8e9a39d90ea9f3399
Hackintosh 5 <hackintosh5@wildfyre.net>
2019-05-03 16:53:54 +0100

    Modularise

--- logo-xiaomi.py
+++ logo-xiaomi.py
@@ -14,47 +14,47 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
 HEADER = b'LOGO!!!!'
 BMP_HEADER = b'BM'
 
 from struct import *
+import sys
 
-f = open('logo.img', 'rb')
-binary = f.read()
+def extract(f):
+    binary = f.read()
+    (header,) = unpack("<8s", binary[0x4000:0x4008])
+    if header != HEADER:
+        print("wrong header", header)
+        sys.exit(1)
 
-(header,) = unpack("<8s", binary[0x4000:0x4008])
-if header != HEADER:
-    print("wrong header", header)
-    sys.exit(1)
+    img_offset = 0
+    img_offsets = []
+    cbyte = 0x4008
 
-img_offset = 0
-img_offsets = []
-cbyte = 0x4008
+    while True:
+        last_offset = img_offset
+        (img_offset,) = unpack("<I", binary[cbyte:cbyte+4])
+        cbyte += 4
+        if img_offset == 0:
+            break
+        else:
+            if (img_offset + last_offset) * 0x1000 in img_offsets:
+                # Sometimes they show up twice. Skip.
+                continue
+            img_offsets.append((img_offset + last_offset) * 0x1000)
 
-while True:
-    last_offset = img_offset
-    (img_offset,) = unpack("<I", binary[cbyte:cbyte+4])
-    cbyte += 4
-    if img_offset == 0:
-        break
-    else:
-        if (img_offset + last_offset) * 0x1000 in img_offsets:
-            # Sometimes they show up twice. Skip.
+    # The last one is the end of the file. Skip.
+    img_offsets.pop()
+
+    # Formatted as BMP. Detect file length
+    img_cnt = 0
+    outs = []
+    for img_offset in img_offsets:
+        img_cnt += 1
+        (header, size) = unpack("<2sI", binary[img_offset:img_offset+6])
+        if header != BMP_HEADER:
+            print('corrupt bmp header skipping')
             continue
-        img_offsets.append((img_offset + last_offset) * 0x1000)
-
-# The last one is the end of the file. Skip.
-img_offsets.pop()
-
-# Formatted as BMP. Detect file length
-img_cnt = 0
-for img_offset in img_offsets:
-    img_cnt += 1
-    (header, size) = unpack("<2sI", binary[img_offset:img_offset+6])
-    if header != BMP_HEADER:
-        print('corrupt bmp header skipping')
-        continue
-    outbuff = binary[img_offset:img_offset+size]
-    with open('logo'+str(img_cnt)+'.bmp', 'wb') as f:
-        f.write(outbuff)
+        outbuff = binary[img_offset:img_offset+size]
+        outs.append(outbuff)
+    return outs
