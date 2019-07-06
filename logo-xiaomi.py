e6a81f0d75f0ef8f58a365a38d83cf87d929f68f
Hackintosh 5 <hackintosh5@wildfyre.net>
2019-07-06 13:41:55 +0100

    Add support for CLI usage and instructions to edit images in GIMP

--- logo-xiaomi.py
+++ logo-xiaomi.py
@@ -18,22 +18,23 @@ HEADER = b'LOGO!!!!'
 BMP_HEADER = b'BM'
 
 from struct import *
+import argparse, os
 
-def check_header(b):
-    (header,) = unpack("<8s", b[0x4000:0x4008])
+def check_header(f):
+    f.seek(0x4000)
+    (header,) = unpack("<8s", f.read(8))
     if header != HEADER:
         print("wrong header", header)
         raise ValueError("Wrong Header")
 
-def get_offsets(b):
+def get_offsets(f):
     img_offset = 0
     img_offsets = []
-    cbyte = 0x4008
+    f.seek(0x4008)
 
     while True:
         last_offset = img_offset
-        (img_offset,) = unpack("<I", b[cbyte:cbyte+4])
-        cbyte += 4
+        (img_offset,) = unpack("<I", f.read(4))
         if img_offset == 0:
             break
         else:
@@ -46,29 +47,27 @@ def get_offsets(b):
     return img_offsets
 
 def extract(f):
-    binary = f.read()
-    check_header(binary)
+    check_header(f)
 
-    img_offsets = get_offsets(binary)
+    img_offsets = get_offsets(f)
 
     # Formatted as BMP. Detect file length
     img_cnt = 0
-    outs = []
     for img_offset in img_offsets:
         img_cnt += 1
-        (header, size) = unpack("<2sI", binary[img_offset:img_offset+6])
+        f.seek(img_offset)
+        (header, size) = unpack("<2sI", f.read(6))
         if header != BMP_HEADER:
             print('corrupt bmp header skipping')
             continue
-        outbuff = binary[img_offset:img_offset+size]
-        outs.append(outbuff)
-    return outs
+        f.seek(img_offset)
+        yield f.read(size)
 
 def edit(orig_imgs, new_imgs):
     binary = bytearray(orig_imgs.read())
-    check_header(binary)
+    check_header(orig_imgs)
 
-    img_offsets = get_offsets(binary)
+    img_offsets = get_offsets(orig_imgs)
 
     if len(img_offsets) != len(new_imgs):
         raise ValueError("All images must be replaced")
@@ -81,8 +80,10 @@ def edit(orig_imgs, new_imgs):
         if header != BMP_HEADER:
             print('corrupt bmp header skipping')
             continue
-        if len(new_imgs[img_cnt]) > size:
-            raise ValueError("Cannot make image larger")
+        next_img = img_offsets[img_cnt+1]-1 if img_cnt+1 < len(img_offsets) else len(binary)
+        if len(new_imgs[img_cnt])+img_offset >= next_img:
+            print(next_img, len(new_imgs[img_cnt]), img_offset)
+            raise ValueError("Image too big; data overlaps with next image")
         (header, size) = unpack("<2sI", new_imgs[img_cnt][:6])
         if header != BMP_HEADER:
             raise ValueError('cannot add corrupt bmp header')
@@ -95,3 +96,40 @@ def edit(orig_imgs, new_imgs):
         img_cnt += 1
 
     return binary
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    subparsers = parser.add_subparsers(required=True)
+
+    parser_extract = subparsers.add_parser("extract")
+    parser_extract.add_argument("-i", "--input", dest='input', action='store', type=argparse.FileType('rb'), required=True)
+    parser_extract.add_argument("-o", "--output", dest='output', action='store', type=str, required=True)
+    parser_extract.set_defaults(func=extract_logo)
+
+    parser_replace = subparsers.add_parser("replace")
+    parser_replace.add_argument("-i", "--input", dest='input', action='store', type=argparse.FileType('rb'), required=True)
+    parser_replace.add_argument("-a", "--image", dest='images', action='append', type=argparse.FileType('rb'), required=True)
+    parser_replace.add_argument("-o", "--output", dest='output', action='store', type=argparse.FileType('wb'), required=True)
+    parser_replace.set_defaults(func=replace_logo)
+    args = parser.parse_args()
+    if hasattr(args, "images"):
+        print(args.func(args.input, args.images, args.output))
+    else:
+        print(args.func(args.input, args.output))
+def extract_logo(input, output):
+    os.makedirs(output, exist_ok=True)
+    c = 0
+    for photo in extract(input):
+        f = open(os.path.join(output, "logo-"+str(c)+".bmp"), "wb")
+        f.write(photo)
+        f.close()
+        c += 1
+
+def replace_logo(input, images, output):
+    out = edit(input, [i.read() for i in images])
+    output.write(out)
+    output.close()
+
+if __name__ == "__main__":
+    main()
